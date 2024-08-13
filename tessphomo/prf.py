1e5b17c1e942b5c43450e1b7bf213bf4a072f386
Tom Barclay <barclay.astro@gmail.com>
2024-08-12 23:37:07 -0400

    Update prf.py

--- tessphomo/prf.py
+++ tessphomo/prf.py
@@ -6,7 +6,7 @@ import os
 
 
 PKG_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
-PRF_FILE_DIRECTORY = PKG_DIRECTORY + './data/mastprf_fitsfiles/'
+PRF_FILE_DIRECTORY = PKG_DIRECTORY + '/data/mastprf_fitsfiles/'
 
 
 class TESS_PRF_Model(object):
